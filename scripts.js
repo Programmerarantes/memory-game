de9189f22539011b3e2e1008f5eb05b365af8e82
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-07-12 12:46:11 -0300

    Faltou resolver bug de clicar 3 cartas rapido demais

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 const gridDisplay = document.querySelector("#grid")
+const insertText = document.querySelector('#insert-text')
+const result = document.querySelector('#result')
+let cardSelect = []
+let cardSelectId = []
+const cardWon = []
 
 
 //the array with the images objects
@@ -33,28 +38,73 @@ const imgsArray = [
 
     {name: 'yu-gi-oh',img:'imgs/yugi.png',},
     
-    {name: 'yu-yu-hakusho', img:'imgs/yuyu.jpg',},
+    {name: 'yu-yu-hakusho', img:'imgs/yuyu.jpg',}
 ]
 
 //Randozime
 imgsArray.sort(() => 0.5 - Math.random())
 
-console.log(imgsArray)
 /*const randomize = () => {
     const cardData = imgsArray()
     cardData.sort(() => Math.random() - 0.5)
     console.log(cardData)
 } */
 
-//
+//printing the imagens on html
 
 function createBoard() {
     for(let i = 0; i < imgsArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'imgs/capa.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
      
 }
 createBoard()
+//
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    const cardOneId = cardSelectId[0]
+    const cardTwoId = cardSelectId[1]
+    console.log(cards)
+    console.log("check for match!")
+    if(cardOneId == cardTwoId) {
+        cards[cardOneId].setAttribute('src', 'imgs/capa.png')
+        cards[cardTwoId].setAttribute('src', 'imgs/capa.png')
+        alert("You have clicked the same image")
+    }
+    if(cardSelect[0] == cardSelect[1]) {
+        alert("You found a match!")
+        cards[cardOneId].setAttribute('src', 'imgs/white.png')
+        cards[cardTwoId].setAttribute('src', 'imgs/white.png')
+        cards[cardOneId].removeEventListener('click', flipCard)
+        cards[cardTwoId].removeEventListener('click', flipCard)
+        cardWon.push(cardSelect)
+    } else {
+        cards[cardOneId].setAttribute('src', 'imgs/capa.png')
+        cards[cardTwoId].setAttribute('src', 'imgs/capa.png')
+        alert("Sorry try again")
+    }
+    result.textContent = cardWon.length
+    cardSelect = []
+    cardSelectId = []
+
+    if(cardWon.length == imgsArray.length/2) {
+        result.innerHTML = "Congratulations!"
+    }
+}
+
+
+//
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardSelect.push(imgsArray[cardId].name)
+    cardSelectId.push(cardId)
+    this.setAttribute('src', imgsArray[cardId].img)
+    if(cardSelect.length === 2) {
+        setTimeout( checkMatch, 1200)
+    }
+
+}
